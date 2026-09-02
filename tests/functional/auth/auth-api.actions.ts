e7490cc1042344dc0ac2bd4bcb745a4d60ab4fa1
kitz wang <mastahkitz@gmail.com>
2026-09-02 15:00:13 +0800

    added capture of access token in preparation for other api tests

--- tests/functional/auth/auth-api.actions.ts
+++ tests/functional/auth/auth-api.actions.ts
@@ -1,6 +1,6 @@
-import { APIRequestContext } from '@playwright/test';
+import { APIRequestContext, APIResponse } from '@playwright/test';
 import { sendApiRequest } from '../utils/api.utils';
-import { LoginRequestBody } from './auth-api.data';
+import { LoginRequestBody, LoginResponseBody } from './auth-api.data';
 
 export async function sendLoginRequest(request: APIRequestContext, body: LoginRequestBody) {
   return sendApiRequest(request, {
@@ -9,4 +9,9 @@ export async function sendLoginRequest(request: APIRequestContext, body: LoginRe
     headers: { 'Content-Type': 'application/json' },
     body,
   });
+}
+
+export async function captureAccessToken(response: APIResponse): Promise<string> {
+  const body: LoginResponseBody = await response.json();
+  return body.data.accessToken;
 }
